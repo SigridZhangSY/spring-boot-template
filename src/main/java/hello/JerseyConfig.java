c2452d5fd1ed31dfd7e367f546ee0a8c38041cb2
syzhang <sigridzhang0603@163.com>
2018-04-18 11:21:13 +0800

    add jersey config to unwrap bad response

--- src/main/java/hello/JerseyConfig.java
+++ src/main/java/hello/JerseyConfig.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.stereotype.Component;
 
 //Component让spring boot扫描到JerseyConfig
@@ -11,6 +12,7 @@ public class JerseyConfig  extends ResourceConfig {
         //        register(UsersResource.class);
 //        配置扫描的restful package.
         packages("hello.web.rest");
-
+        //不加此配置jersey会将错误码response包装成404
+        property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true);
     }
 }
