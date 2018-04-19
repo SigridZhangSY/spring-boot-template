f7deb2c6cba8d9a8d82ce585d71a526d95c2841f
syzhang <sigridzhang0603@163.com>
2018-04-19 09:08:17 +0800

    update JerseyConfig

--- src/main/java/hello/JerseyConfig.java
+++ src/main/java/hello/JerseyConfig.java
@@ -1,5 +1,6 @@
 package hello;
 
+import hello.web.rest.UsersResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,10 @@ public class JerseyConfig  extends ResourceConfig {
 
         //        register(UsersResource.class);
 //        配置扫描的restful package.
-        packages("hello.web.rest");
+//        packages("hello.web.rest");
+
+        //通过指定package路径配置要注册的resource类，在打包时目录结构改变，会找不到相应的类
+        register(UsersResource.class);
         //不加此配置jersey会将错误码response包装成404
         property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true);
     }
